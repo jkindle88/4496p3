149deaf5ffa4453dd7a5fc014627c47e57528915
George Munoz <gmunoz3@gatech.edu>
2012-04-01 17:32:28 -0400

    h8 you, git

--- main.cpp
+++ main.cpp
@@ -177,13 +177,13 @@ void MotionCallback(int x, int y)
     int dy = my - y;
     
 	//DEBUG -- Remove in final version
-    gCameraForward.normalize();
+    /*gCameraForward.normalize();
     gCameraRight.cross(gCameraForward,NxVec3(0,1,0));
 	
     NxQuat qx(NxPiF32 * dx * 20 / 180.0f, NxVec3(0,1,0));
     qx.rotate(gCameraForward);
     NxQuat qy(NxPiF32 * dy * 20 / 180.0f, gCameraRight);
-    qy.rotate(gCameraForward);
+    qy.rotate(gCameraForward);*/
 	
     mx = x;
     my = y;
